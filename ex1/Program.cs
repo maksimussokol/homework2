d7774f62b28cf5df367329d583179d5db21339e8
maksimussokol <maksimus@2074.ru>
2022-09-20 20:25:34 +0500

    ex1 works

---
+++ ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("enter number between 100 and 999");
+int n = Convert.ToInt32(Console.ReadLine());
+int f = Convert.ToInt32(n / 10);
+int z = Convert.ToInt32(f % 10);
+
+if (n < 100 || n > 999)
+{
+    Console.WriteLine("error");
+}
+else
+{
+    Console.Write("middle point is ");
+    Console.WriteLine(z);
+}
+
+
