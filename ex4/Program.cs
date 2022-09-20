d3d5dc50bbab8ee1eea9127ec748b560ba0d7f0d
maksimussokol <maksimus@2074.ru>
2022-09-20 22:17:17 +0500

    ex4 hihuya ne works

---
+++ ex4/Program.cs
@@ -0,0 +1,33 @@
+﻿// я НЕ понимаю почему не складывает последний элемент!
+
+Console.Clear();
+Console.WriteLine("vvedi skolko kustov");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1 || n > 1000)
+{
+    Console.WriteLine("error");
+}
+Console.WriteLine("vvedi massiv kolichestva yagod na kustah");
+
+int[] a = new int[n];
+int i = 0;
+while (i < n)
+{
+    a[i] = Convert.ToInt32(Console.ReadLine());
+    i = i + 1;
+}
+
+int max = a[1] + a[2] + a[3];
+while (i < n)
+{
+    if ((a[i] + a[i + 1] + a[i + 2] > max))
+    {
+        max = a[i] + a[i + 1] + a[i + 2];
+        i++;
+
+    }
+}
+Console.Write("maksimalno za odin prohod ");
+Console.WriteLine(max);
+
+
